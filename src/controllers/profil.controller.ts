1e6a988f0fa6477309428fb63c890a772ecfc6ad
Quentin Gallam Detré <quentindetre@yahoo.com>
2025-05-27 15:42:46 +0200

    fix[dw2-15]: added 'export class' to 'getPlayerById' function.

--- src/controllers/profil.controller.ts
+++ src/controllers/profil.controller.ts
@@ -1,23 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import { db } from '../config/models/index';
 
-// ! AFFICHAGE D'UN JOUEUR PAR ID ! \\
 
-export const getPlayerbyId = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { id } = req.params;
+export class ProfilController {
+    // ! AFFICHAGE D'UN JOUEUR PAR ID ! \\
 
-        // * "user" DANS "db.user.findByPk(id);" À MODIFIER SELON SYNTAXE CHOISIE
-        const player = await db.player.findByPk(id);
-    
-        if (!player) {
-            const error = new Error("User not found !");
-            (error as any).status = 404;
-            throw error;
+    static async getPlayerbyId(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
+
+            // * "User" DANS "db.User.findByPk(id);" À MODIFIER SELON SYNTAXE CHOISIE
+            const user = await db.User.findByPk(id);
+
+            if (!user) {
+                const error = new Error("Impossible de trouver cet utilisateur !");
+                (error as any).status = 404;
+                throw error;
+            }
+
+            res.status(200).json(user);
+        } catch (error) {
+            next(error);
         }
-    
-        res.status(200).json(player);
-    } catch (error) {
-        next(error);
     }
 }
