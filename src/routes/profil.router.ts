dc9e892bb8b9970ba1c1b3cd5244b9ad6df6b7ad
Vaganuki <hugo.warnotte@gmail.com>
2025-05-27 14:57:06 +0200

    fix(dw2-25): index et profil route

--- src/routes/profil.router.ts
+++ src/routes/profil.router.ts
@@ -1,11 +1,2 @@
-import { Router } from 'express';
-import { ProfilController } from '../controllers/profil.controller';
-import { authenticateToken } from '../middlewares/auth.middleware';
+export const profilRouter = require('express').Router();
 
-const router = Router();
-const profilController = new ProfilController();
-
-
-router.get('/profile', authenticateToken, profilController.profilConnection);
-
-export default router;
