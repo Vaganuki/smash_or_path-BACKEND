264756b958f24a5cd9c7484cf689b56de1e4f231
Vaganuki <hugo.warnotte@gmail.com>
2025-05-27 14:56:36 +0200

    fix(dw2-25): app.ts propre

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { routes } from './routes';
+// import db from './config/database';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('listening port 3000');
+});
