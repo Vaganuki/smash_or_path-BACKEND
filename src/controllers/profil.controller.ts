f11c34954f3bbffc1203c9c4d7936523a36a8ac1
Quentin Gallam Detré <quentindetre@yahoo.com>
2025-05-27 10:11:44 +0200

    feat[DW2-15]: getById function for users.

---
+++ src/controllers/profil.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { db } from '../config/models/index';
+
+// ! AFFICHAGE D'UN JOUEUR PAR ID ! \\
+
+export const getPlayerbyId = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+
+        // * "User" DANS "db.User.findByPk(id);" À MODIFIER SELON SYNTAXE CHOISIE
+        const user = await db.User.findByPk(id);
+    
+        if (!user) {
+            const error = new Error("Impossible de trouver cet utilisateur !");
+            (error as any).status = 404;
+            throw error;
+        }
+    
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+}
