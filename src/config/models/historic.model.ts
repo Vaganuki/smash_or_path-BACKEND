5b3e25ea389bc909463c39ace39d79c5cf40ba9a
morganfolloni <le-follo@hotmail.com>
2025-05-28 09:33:59 +0200

    dw2-17 (testing push (model))

---
+++ src/config/models/historic.model.ts
@@ -0,0 +1,9 @@
+export interface HistoricModel {
+    id: number;
+    player1: string;
+    player2: string;
+    characterPlayer1: string;
+    characterPlayer2: string;
+    winningPlayer: string;
+    remainingLifes: number;
+}
