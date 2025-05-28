a53a7d6113e33dc62592fdcc2b4a8cec3c771bf2
Vaganuki <hugo.warnotte@gmail.com>
2025-05-28 11:13:43 +0200

    feat(dw2-37): added player and eats models

---
+++ src/config/models/eats.model.ts
@@ -0,0 +1,23 @@
+import {Eats, Player} from "../../@types/types";
+import { DataTypes, Sequelize, Model } from "sequelize";
+
+export default (sequelize: Sequelize) => {
+    const EatsModel = sequelize.define<Eats>(
+        "eats",
+        {
+            player_id: {
+                player_id : DataTypes.INTEGER,
+                allowNull: true
+            },
+            banana_id: {
+                type: DataTypes.INTEGER,
+                allowNull: true
+            }
+        },
+        {
+            tableName: "eats", // Nom de la table dans la base de données
+            timestamps: false // Pas de colonnes createdAt/updatedAt
+        }
+    );
+    return EatsModel;
+};
