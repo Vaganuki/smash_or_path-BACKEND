c18cf96e380fde6dd4bc0accf9cf0a5b7f720d43
FlorianGas <gasparflorian@gmail.com>
2025-05-28 10:37:37 +0200

    feat(dw2-37):Player Model and @Types

---
+++ src/@types/types.d.ts
@@ -0,0 +1,83 @@
+export interface Db{
+    sequelize: Sequelize;
+    player: ModelStatic<Player>;
+    banana: ModelStatic<Banana>;
+    champion: ModelStatic<Champion>;
+    skin: ModelStatic<Skin>;
+    fight: ModelStatic<Fight>;
+    tournament: ModelStatic<Tournament>;
+    history: ModelStatic<History>;
+    tournament_history: ModelStatic<TournamentHistory>;
+    prefers: ModelStatic<Prefers>;
+    eats: ModelStatic<Eats>;
+    plays: ModelStatic<Plays>;
+    participates: ModelStatic<Participates>;
+}
+export interface Champion extends Model<InferAttributes<Champion>, InferCreationAttributes<Champion>> {
+    champion_id: CreationOptional<number>;
+    name: string;
+}
+export interface Banana extends Model<InferAttributes<Banana>, InferCreationAttributes<Banana>> {
+    banana_id: CreationOptional<number>;
+    brand: string;
+}
+export interface Player extends Model<InferAttributes<Player>, InferCreationAttributes<Player>> {
+    player_id: CreationOptional<number>;
+    email: string;
+    pseudo: string;
+    password: string;
+    elo: string;
+    tournament_won: number;
+    favorite_champion_id: ForeignKey<Champion["champion_id"]>;
+    hated_champion_id: ForeignKey<Champion["champion_id"]>;
+    is_active: boolean;
+}
+export interface Skin extends Model<InferAttributes<Skin>, InferCreationAttributes<Skin>> {
+    skin_id: CreationOptional<number>;
+    champion_id: ForeignKey<Champion["champion_id"]>;
+    image: string;
+}
+export interface Fight extends Model<InferAttributes<Fight>, InferCreationAttributes<Fight>> {
+    fight_id: CreationOptional<number>;
+    player1_id: ForeignKey<Player["player_id"]>;
+    player2_id: ForeignKey<Player["player_id"]>;
+    champion_p1_id: ForeignKey<Champion["champion_id"]>;
+    champion_p2_id: ForeignKey<Champion["champion_id"]>;
+    score_p1: number;
+    score_p2: number;
+    winner_player_id: ForeignKey<Player["player_id"]>;
+}
+export interface Tournament extends Model<InferAttributes<Tournament>, InferCreationAttributes<Tournament>> {
+    tournament_id: CreationOptional<number>;
+    winner_tournament_player_id: ForeignKey<Player["player_id"]>;
+    first_place_player_id: ForeignKey<Player["player_id"]>;
+    second_place_player_id: ForeignKey<Player["player_id"]>;
+    third_place_player_id: ForeignKey<Player["player_id"]>;
+    first_place: number;
+    second_place: number;
+    third_place: number;
+}
+export interface History extends Model<InferAttributes<History>, InferCreationAttributes<History>> {
+    history_id: CreationOptional<number>;
+    fight_id: ForeignKey<Fight["fight_id"]>;
+}
+export interface TournamentHistory extends Model<InferAttributes<TournamentHistory>, InferCreationAttributes<TournamentHistory>> {
+    tournament_history_id: CreationOptional<number>;
+    tournament_id: ForeignKey<Tournament["tournament_id"]>;
+}
+export interface Prefers extends Model<InferAttributes<prefers>, InferCreationAttributes<prefers>> {
+    player_id: ForeignKey<Player["player_id"]>;
+    champion_id: ForeignKey<Champion["champion_id"]>;
+}
+export interface Eats extends Model<InferAttributes<eats>, InferCreationAttributes<eats>> {
+    player_id: ForeignKey<Player["player_id"]>;
+    banana_id: ForeignKey<Banana["banana_id"]>;
+}
+export interface Plays extends Model<InferAttributes<plays>, InferCreationAttributes<plays>> {
+    player_id: ForeignKey<Player["player_id"]>;
+    fight_id: ForeignKey<Fight["fight_id"]>;
+}
+export interface Participates extends Model<InferAttributes<participates>, InferCreationAttributes<participates>> {
+    player_id: ForeignKey<Player["player_id"]>;
+    tournament_id: ForeignKey<Tournament["tournament_id"]>;
+}
---
+++ src/config/models/Player.ts
@@ -0,0 +1,59 @@
+import {Player} from "../../@types/types";
+import { DataTypes, Sequelize, Model } from "sequelize";
+
+export default (sequelize: Sequelize) => {
+    const PlayerModel = sequelize.define<Player>(
+        "player",
+        {
+            player_id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                unique: true
+            },
+            email:{
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true, // L'email doit être unique
+                validate: {
+                    isEmail: true // Validation que la valeur est un email
+                }},
+                pseudo : {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                password : {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                elo:{
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    defaultValue: 0 // Valeur par défaut pour l'Elo
+                },
+                tournament_won :{
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    defaultValue: 0 // Valeur par défaut pour le nombre de tournois gagnés
+                },
+                favorite_champion_id :{
+                    type: DataTypes.INTEGER,
+                    allowNull: true // Peut être nul si aucun champion favori n'est défini
+                },
+                hated_champion_id : {
+                    type: DataTypes.INTEGER,
+                    allowNull: true // Peut être nul si aucun champion détesté n'est défini
+                },
+                is_active   : {
+                    type: DataTypes.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: true // Valeur par défaut pour l'état actif du joueur
+                },
+        },
+        {
+            tableName: "player", // Nom de la table dans la base de données
+            timestamps: false // Pas de colonnes createdAt/updatedAt
+        }
+    );
+    return PlayerModel;
+};
