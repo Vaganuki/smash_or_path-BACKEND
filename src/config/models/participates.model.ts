8243cf5e03f630f6cb0d4e575ba9db24766bb198
Vaganuki <hugo.warnotte@gmail.com>
2025-05-28 14:10:27 +0200

    feat(dw2-41):added participate model

---
+++ src/config/models/participates.model.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Sequelize } from "sequelize";
+import { Participates } from "../../@types/types";
+
+export default (sequelize: Sequelize) => {
+    const ParticipatesModel = sequelize.define<Participates>(
+        "Participates",
+        {
+            player_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "player",
+                    key: "player_id"
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+            tournament_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "tournament",
+                    key: "tournament_id"
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            }
+        },
+        {
+            tableName: "participates",
+            timestamps: false,
+            indexes: [
+                {
+                    unique: true,
+                    fields: ['player_id', 'tournament_id']
+                }
+            ]
+        }
+    );
+    
+    return ParticipatesModel;
+};
